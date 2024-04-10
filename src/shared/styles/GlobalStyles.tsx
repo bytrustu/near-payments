38f98f17629d27509e842943e44e1a0663323733
bytrustu <bytrustu@gmail.com>
2024-04-10 21:49:30 +0900

    feat: 라이브러리 빌드를 위한 Vite 설정 추가

--- src/shared/styles/GlobalStyles.tsx
+++ src/shared/styles/GlobalStyles.tsx
@@ -1,13 +1,9 @@
 import { Global, css } from '@emotion/react';
 
-export const GlobalStyles = () => (
-  <Global
-    styles={css`
-      *,
-      *::before,
-      *::after {
+export const GlobalStyles = () => <Global styles={css`
+    *,
+    *::before,
+    *::after {
         box-sizing: border-box;
-      }
-    `}
-  />
-);
+    }
+`} />;
